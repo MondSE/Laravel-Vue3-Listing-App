33627c089194f3a2fac934e47fe1d92a1761679c
Almond Tuazon <azirtuazon@gmail.com>
2024-10-23 12:22:38 +0800

    created Home page created auth folder to Register Page create Components folder under Components folder NavLink,Container,Title,TextLink,InputField,PrimaryBtn

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::inertia('/', 'Home')->name('home');
+
+Route::get('/register', [RegisterController::class, 'create'])->name('register');
